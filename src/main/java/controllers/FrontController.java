d1a6d3951a3d17ccd3b5def5f868d89f12446710
yukimi.yamada <yukimi2356@gmail.com>
2022-03-16 23:29:58 +0900

    FrontController作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="FrontController", urlPatterns={"/"})
+import actions.ActionBase;
+import actions.UnknownAction;
+import constants.ForwardConst;
+
+@WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -17,11 +22,38 @@ public class FrontController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ActionBase action = getAction(request, response);
+
+        action.init(getServletContext(),request,response);
+
+        action.process();
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
+    }
+
+    @SuppressWarnings({"rawtypes","unchecked"})
+    private ActionBase getAction(HttpServletRequest request, HttpServletResponse response) {
+        Class type = null;
+        ActionBase action = null;
+        try {
+            String actionString = request.getParameter(ForwardConst.ACT.getValue());
+
+            type = Class.forName(String.format("actions.%sAction", actionString));
+
+            action = (ActionBase)(type.asSubclass(ActionBase.class)
+                    .getDeclaredConstructor()
+                    .newInstance());
+
+        }catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException
+                | IllegalArgumentException | InvocationTargetException| NoSuchMethodException e) {
+
+            action = new UnknownAction();
+
+        }
+        return action;
     }
 
 }
