102d1301a3099a724ea99a3dafea5ee133abae5d
yukimi.yamada <yukimi2356@gmail.com>
2022-03-19 23:08:33 +0900

    従業員showアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -116,6 +116,25 @@ public class EmployeeAction extends ActionBase {
     }
 
     /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+
+    public void show() throws ServletException, IOException{
+        EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+        if(ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return;
+        }
+
+        putRequestScope(AttributeConst.EMPLOYEE, ev);
+
+        forward(ForwardConst.FW_EMP_SHOW);
+    }
+
+    /**
      * 編集画面を表示する
      * @throws ServletException
      * @throws IOException
