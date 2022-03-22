2bac712b5458f7df4e645c7716533cb5dca5a3c5
yukimi.yamada <yukimi2356@gmail.com>
2022-03-23 01:08:29 +0900

    日報createアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.ReportService;
 
 /**
@@ -59,6 +61,47 @@ public class ReportAction extends ActionBase {
         putRequestScope(AttributeConst.REPORT, rv);
 
         forward(ForwardConst.FW_REP_NEW);
+    }
+
+    public void create() throws ServletException, IOException {
+
+        if(checkToken()) {
+
+            LocalDate day = null;
+            if(getRequestParam(AttributeConst.REP_DATE) == null
+                    || getRequestParam(AttributeConst.REP_DATE).equals("")) {
+                day = LocalDate.now();
+            }else {
+                day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+            }
+
+            EmployeeView ev = (EmployeeView)getSessionScope(AttributeConst.LOGIN_EMP);
+
+            ReportView rv = new ReportView(
+                    null,
+                    ev,
+                    day,
+                    getRequestParam(AttributeConst.REP_TITLE),
+                    getRequestParam(AttributeConst.REP_CONTENT),
+                    null,
+                    null);
+
+            List<String> errors = service.create(rv);
+
+            if(errors.size() > 0 ) {
+                putRequestScope(AttributeConst.TOKEN, getTokenId());
+                putRequestScope(AttributeConst.REPORT, rv);
+                putRequestScope(AttributeConst.ERR, errors);
+
+                forward(ForwardConst.FW_REP_NEW);
+
+            }else {
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+
+                redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
+            }
+
+        }
     }
 
 
