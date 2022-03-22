5abcf69a29a62d66541de97af9bd6631ffb09cfc
yukimi.yamada <yukimi2356@gmail.com>
2022-03-23 00:42:37 +0900

    日報indexアクションの作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,54 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+/**
+ * 日報に関する処理を行うActionクラス
+ *
+ */
+public class ReportAction extends ActionBase {
+
+    private ReportService service;
+
+    @Override
+    public void process() throws ServletException, IOException {
+        service = new ReportService();
+
+        invoke();
+        service.close();
+    }
+
+    public void index() throws ServletException, IOException {
+
+        int page = getPage();
+        List<ReportView> reports = service.getAllPerPage(page);
+
+        long reportsCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if(flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        forward(ForwardConst.FW_REP_INDEX);
+    }
+
+
+
+}
