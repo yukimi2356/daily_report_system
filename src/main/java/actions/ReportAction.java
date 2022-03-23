b85b2ed8fad5b9db326788179004bf7a78052ea6
yukimi.yamada <yukimi2356@gmail.com>
2022-03-23 20:37:01 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -100,7 +100,21 @@ public class ReportAction extends ActionBase {
 
                 redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
             }
+        }
 
+    }
+
+        public void show() throws ServletException, IOException {{
+
+            ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+            if(rv == null) {
+                forward(ForwardConst.FW_ERR_UNKNOWN);
+            }else {
+                putRequestScope(AttributeConst.REPORT, rv);
+
+                forward(ForwardConst.FW_REP_SHOW);
+            }
         }
     }
 
